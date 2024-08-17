b66ad392efb1f1154305e309d4b10c9fc7303181
galbb12 <galbb12@gmail.com>
2024-08-17 16:40:59 +0300

    Added far return to update code segment

--- kernel/arch/x86_64/gdt/load_gdt.s
+++ kernel/arch/x86_64/gdt/load_gdt.s
@@ -1,4 +1,5 @@
 
+[bits 64]
 [global load_gdt]
 gdtr DW 0 ; For limit storage
      DQ 0 ; For base storage
@@ -7,15 +8,20 @@ load_gdt:
     cli
     mov [gdtr], di
     mov [gdtr+2], rsi
-    lgdt [gdtr]         ; Load GDT Register with LGDT 
-    sub rsp, 8 
+    lgdt [gdtr]         ; Load GDT Register with LGDT
+    push 0x10
+    lea rax, [rel reload_cs]
+    ;sub sp, 8
+    ;mov [rsp], rax
+    push rax
+    lretfq 
 
+reload_cs:
     mov ax, 0x20
     mov ds, ax         ; Reload all the data descriptors with Data selector  
     mov es, ax
     mov gs, ax
     mov fs, ax
     mov ss, ax
-
-    add rsp, 8  
+    add sp, 0x10
     ret
