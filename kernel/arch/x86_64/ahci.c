ab6d3a1044fa387789c4a1dd982b0104ea44ae25
galbb12 <galbb12@gmail.com>
2024-09-03 23:03:56 +0300

    Fixed incorrect statement.

--- kernel/arch/x86_64/ahci.c
+++ kernel/arch/x86_64/ahci.c
@@ -5,7 +5,7 @@ linkedListNode* setup_ahci_controllers() {
   linkedListNode* out = NULL;
   while (head) {
     PCIDevice *device = (PCIDevice *)head->data;
-    if (device->classCode == 0x06 && (device->subclass == 0x06 || device->subclass == 0x01)) {
+    if ((device->classCode == 0x06 || device->classCode == 0x01) && device->subclass == 0x06) {
       assign_bar(*device, AHCI_CONFIG_BAR_NUM);
       printf("Found AHCI controller %x:%x", device->bus, device->slot);
       append_node(&out, device);
